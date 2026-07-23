2a06d4973ca7e63ee88e3830e70846a7db76259f
Muca <murilo301c@gmail.com>
2026-07-23 11:28:43 -0300

    To do list

---
+++ app/backend/spec/static/js/script.js
@@ -0,0 +1,18 @@
+function adicionarTarefa(){
+    let Vinput = document.querySelector("input").value
+
+    // Evita adicionar tarefas em branco
+    if (Vinput.trim() === "") return;
+
+    let Fli = document.createElement("li")
+    document.querySelector("ul").appendChild(Fli)
+    
+    Fli.innerHTML = Vinput + '<span onclick="deletarTarefa(this)">  ❌</span>'
+
+    document.querySelector("input").value = ""
+}
+
+function deletarTarefa(elementoClicado){
+    // O parentElement do span ❌ é o <li> que precisamos apagar
+    elementoClicado.parentElement.remove()
+}
